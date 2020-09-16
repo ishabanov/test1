d9bb8d098987ea8df2b36c3387666905ffe35d36
ishabanov <igor.shabanov0277@gmail.com>
2020-09-16 23:34:24 +0300

    change new_file4.py

---
+++ new_file4.py
@@ -0,0 +1,33 @@
+# l = [1, 2, 3, 4, 5, 6]
+
+# l[1], l[-2] = l[-2], l[1]
+
+# print(l)
+
+# a = input("Enter number:")
+# a = int(a)
+
+# if a > 100:
+#     print("Good!")
+#     s = str(a)
+#     print(s)
+# elif a > 0 and a <= 100:
+#     print("Ok")
+# else:
+#     print("Bad")
+
+
+#Practice 1
+
+# a = input("Enter 'Hi'")
+# if a == "Hi":
+#     print("Hello!")
+# print("Goodbye!")
+
+#Practice 2
+
+x = int(input("Enter a number: "))
+if 0 < x <= 100:
+    print("Correct number")
+else:
+    print("Wrong number")
